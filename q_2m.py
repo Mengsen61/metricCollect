b1654c1ffedd9dd9e766b1388431fdfe216a3ce1
Mengsen61 <jubuge@gmail.com>
2019-09-19 13:28:39 +0800

    yoyo

--- q_2m.py
+++ q_2m.py
@@ -3,10 +3,10 @@ from elasticsearch import Elasticsearch
 import pymysql, requests
 import settings
 
-def alert_push_group(s_cluster, s_app, s_appId, s_classname, s_formId,s_opMethod, s_timestamp, s_count):
+def alert_push_group(s_measurement,s_cluster, s_app, s_appId, s_classname, s_formId,s_opMethod, s_timestamp, s_count):
     url = "http://127.0.0.1:8086/write?db=test"
     #print(time*10**6)
-    payload = 'ERROR_op,clusterName=%s,appName=%s,appId=%s,className=%s,formId=%s,opMethod=%s count=%d %d' % (s_cluster,s_app,s_appId,s_classname,s_formId,s_opMethod,s_count,s_timestamp*10**6)
+    payload = '%s,clusterName=%s,appName=%s,appId=%s,className=%s,formId=%s,opMethod=%s count=%d %d' % (s_measurement,s_cluster,s_app,s_appId,s_classname,s_formId,s_opMethod,s_count,s_timestamp*10**6)
     response = requests.post(url, data=payload.encode('utf-8'))
     print(response.status_code)
     #print(response.headers)
@@ -17,6 +17,7 @@ class Metric(object):
         self.interval = settings.interval
         self.eshost = settings.eshost
         self.dbconfig = settings.dbconfig
+        self.timerange = settings.timerange
         for k,v in kwargs.items():
             setattr(self,k,v)
 
@@ -100,12 +101,14 @@ class Metric(object):
                                                             },
                                                             "aggs":{}}}}}}}}}}}}}}}}
         print(body)
-        response = client.search(
+
+        body_1 = {"size":0,"query":{"bool":{"filter":[{"range":{"@timestamp":{"gte":"now-%s" % self.timerange,"lte":"now","format":"epoch_millis"}}},{"query_string":{"analyze_wildcard":true,"query":"*"}}]}},"aggs":{"5":{"terms":{"field":"clusterName.keyword","size":10,"order":{"_key":"desc"},"min_doc_count":1},"aggs":{"6":{"terms":{"field":"appName.keyword","size":10,"order":{"_key":"desc"},"min_doc_count":1},"aggs":{"7":{"terms":{"field":"logtags.appId.keyword","size":10,"order":{"_key":"desc"},"min_doc_count":1},"aggs":{"8":{"terms":{"field":"className.keyword","size":10,"order":{"_key":"desc"},"min_doc_count":1},"aggs":{"9":{"date_histogram":{"interval":"%s" % self.interval,"field":"@timestamp","min_doc_count":0,"extended_bounds":{"min":"now-%s" % self.timerange,"max":"now"},"format":"epoch_millis"},"aggs":{}}}}}}}}}}}} 
+        response_1 = client.search(
             index="*",
             body=body
         )
 
-        res_list = response.get('aggregations').get('6').get('buckets')
+        res_list = response_1.get('aggregations').get('6').get('buckets')
         #print(res_list)
 
         for cluster in res_list:
@@ -116,6 +119,7 @@ class Metric(object):
                     s_appId = appId.get('key')
                     for className in appId.get('9').get('buckets'):
                         s_classname = className.get('key')
+
                         for formId in className.get('10').get('buckets'):
                             s_formId = formId.get('key')
                             for opMethod in formId.get('3').get('buckets'):
@@ -130,8 +134,35 @@ class Metric(object):
                                         s_count = timestamp.get('doc_count')
                                 #alert_push_group(s_cluster,s_app,s_classname,self.keyword,s_level,s_count,s_timestamp)
                                 #self.update_mysql(s_cluster, s_app, s_classname, s_level, s_timestamp, s_count)
-                                print(s_cluster, s_app, s_appId, s_classname, s_formId,s_opMethod, s_timestamp, s_count)
-                                alert_push_group(s_cluster, s_app, s_appId, s_classname, s_formId,s_opMethod, s_timestamp, s_count)
+                                        print(s_cluster, s_app, s_appId, s_classname, s_formId,s_opMethod, s_timestamp, s_count)
+                                        alert_push_group("ERROR_op", s_cluster, s_app, s_appId, s_classname, s_formId,s_opMethod, s_timestamp, s_count)
+        response_2 = client.search(
+            index="*",
+            body=body_1
+            )
+        res_list_2 = response_2.get('aggregations').get('5').get('buckets')
+        #print(res_list)
+
+        for cluster2 in res_list_2:
+            s_cluster2 = cluster2.get('key')
+            for app2 in cluster2.get('6').get('buckets'):
+                s_app2 = app2.get('key')
+                for appId2 in app2.get('7').get('buckets'):
+                    s_appId2 = appId2.get('key')
+                    for className2 in appId2.get('8').get('buckets'):
+                        s_classname2 = className2.get('key')
+                        loop_count2 = 0
+                        for timestamp2 in className2.get('9').get('buckets'):  
+                            if loop_count2 == 0:
+                                loop_count2 += 1
+                                continue
+                            else:
+                                s_timestamp2 = timestamp2.get('key')
+                                s_count2 = timestamp2.get('doc_count')
+                                print(s_cluster2, s_app2, s_appId2, s_classname2, s_timestamp2, s_count2)
+                                alert_push_group("ERROR", s_cluster2, s_app2, s_appId2, s_classname2, "null","null", s_timestamp2, s_count)                 
+
+
 
 if __name__ == "__main__":
     metric1 = Metric(keyword="*")
--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 interval = "20s"
 eshost = "172.18.3.52:9200"
+timerange = "2m"
+
+
 dbconfig = {
-    'host':'10.200.9.21',
-    'user':'mc_prod_esmetirc_user',
+    'host':'127.0.0.1:8086',
+    'user':'admin',
     'password':'6aCt35vNj0FQAXp5h_esmetirc',
-    'port':3306,
-    'db':'esmetirc',
-    'table':'metrics'
+    'db':'test',
 }
